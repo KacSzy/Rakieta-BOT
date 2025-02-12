92ad7c6ec9042e4b3e986e28167d0d8bfa6365cb
KacSzy <kacper.szymanski.22@gmail.com>
2025-02-12 22:22:32 +0100

    project structure

---
+++ const.py
@@ -0,0 +1,30 @@
+BLOCKED_WORDS = [
+    'm4lol4tami',
+]
+BLOCKED_LINKS = [
+    'steamcommunity.com/gift/activation/id=',
+]
+
+ALLOWED_ROLES_TO_POST_LINKS = [
+    '15 lvl',
+    '20 lvl',
+    '25 lvl',
+    '30 lvl',
+    '40 lvl',
+    '50 lvl',
+    '60 lvl',
+    '70 lvl',
+    '80 lvl',
+    '90 lvl',
+    '100 lvl',
+    '110 lvl',
+    '120 lvl',
+    '130 lvl',
+    '140 lvl',
+    '150 lvl',
+    '160 lvl',
+    '170 lvl',
+    '180 lvl',
+    '190 lvl',
+    'Legenda Serwera',
+]
