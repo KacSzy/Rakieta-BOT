3beb9c0bc24c12dad58294217ce48032c1975df5
KacSzy <kacper.szymanski.22@gmail.com>
2025-02-17 18:39:50 +0100

    log_channel added

--- const.py
+++ const.py
@@ -2,8 +2,11 @@ BLOCKED_WORDS = [
     'm4lol4tami',
     'm4lo4tam!',
 ]
+
 BLOCKED_LINKS = [
     'steamcommunity.com/gift/activation/id=',
     'https://steamecomnmunity.com/',
     'steam gift 50$',
 ]
+
+LOG_CHANNEL_ID = 1053819227741638756
