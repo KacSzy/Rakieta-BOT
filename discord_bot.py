255cecf07bb9c1d67148dd066fc785e8ef1ac4f1
KacSzy <kacper.szymanski.22@gmail.com>
2025-02-20 20:19:21 +0100

    Pre-update 2.0

--- discord_bot.py
+++ discord_bot.py
@@ -18,8 +18,9 @@ bot = commands.Bot(command_prefix="_", intents=intents)
 
 @bot.event
 async def on_ready():
-    activity = discord.Game(name="Ziuuuu")
-    await bot.change_presence(status=discord.Status.online, activity=activity)
+    # activity = discord.Game(name="Ziuuuu")
+    # await bot.change_presence(status=discord.Status.online, activity=activity)
+    await bot.change_presence(activity=discord.CustomActivity(name='Nadchodzi coś fajnego... 👀'))
     print(f"Logged as {bot.user}")
     await bot.tree.sync(guild=GUILD)
 
