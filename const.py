c16c17378172230b69f85956006ce05b4b4145f6
KacSzy <kacper.szymanski.22@gmail.com>
2025-02-26 12:11:18 +0100

    2.0.3 scamlink

--- const.py
+++ const.py
@@ -7,7 +7,8 @@ BLOCKED_LINKS = [
     'steamcommunity.com/gift/activation/id=',
     'steamecomnmunity.com/',
     'steam gift 50$',
-    'is.gd/'
+    'is.gd/',
+    'steam-shortlink.xyz'
 ]
 
 LOG_CHANNEL_ID = 1053819227741638756
